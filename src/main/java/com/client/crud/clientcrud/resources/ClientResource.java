4832b9a72f44d8da0741afcd178161ef67a3a022
Wesley Tessaro <wesleyt.andrade@gmail.com>
2022-12-04 19:05:57 -0400

    fixed paged find all

--- src/main/java/com/client/crud/clientcrud/resources/ClientResource.java
+++ src/main/java/com/client/crud/clientcrud/resources/ClientResource.java
@@ -3,6 +3,9 @@ package com.client.crud.clientcrud.resources;
 import com.client.crud.clientcrud.dto.ClientDTO;
 import com.client.crud.clientcrud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,6 +18,22 @@ public class ClientResource {
 
     @Autowired
     private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<ClientDTO> list = service.findAllPaged(pageRequest);
+
+        return ResponseEntity.ok().body(list);
+    }
+
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
